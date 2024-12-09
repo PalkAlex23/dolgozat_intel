7091d9edd628f698bf50f6ede3b54d989ae6f4f6
PalkAlex23 <alexpytmusic@gmail.com>
2024-12-09 14:21:38 +0100

    f1 10

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -2,6 +2,11 @@ package hu.szamalk;
 
 import hu.szamalk.modell.Tabla;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
         Tabla tabla = new Tabla('#');
@@ -21,5 +26,20 @@ public class Main {
         System.out.println("9. feladat: Üres oszlopok és sorok száma:");
         System.out.println("Oszlopok: " + tabla.UresOszlopokSzama());
         System.out.println("Sorok: " + tabla.UresSorokSzama());
+
+        // 10. feladat
+        InputStream is = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }
+        };
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedInputStream bf = new BufferedInputStream(is);
+        Tabla tabla2 = new Tabla('*');
+        for(int i = 0; i < 64; i++) {
+            tabla2.Elhelyez(i);
+            tabla2.Megjelenit();
+        }
     }
 }
