d6f56b3efedbbedff512d8e7aa8ce47c67de7910
PalkAlex23 <alexpytmusic@gmail.com>
2024-12-09 13:54:43 +0100

    f1 6

--- src/main/java/hu/szamalk/Main.java
+++ src/main/java/hu/szamalk/Main.java
@@ -5,8 +5,11 @@ import hu.szamalk.modell.Tabla;
 public class Main {
     public static void main(String[] args) {
         Tabla tabla = new Tabla('#');
+        System.out.println("4. Feladat: Az üres tábla:");
         tabla.Megjelenit();
-        tabla.Elhelyez(5);
+        // 6. feladat
+        System.out.println("6. Feladat: A feltöltött tábla:");
+        tabla.Elhelyez(8);
         tabla.Megjelenit();
     }
 }
