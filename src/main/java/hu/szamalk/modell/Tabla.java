37ff0279401b383b95f14bb61cd35a5a8544fb1e
PalkAlex23 <alexpytmusic@gmail.com>
2024-12-09 13:35:19 +0100

    f1 3

--- src/main/java/hu/szamalk/modell/Tabla.java
+++ src/main/java/hu/szamalk/modell/Tabla.java
@@ -1,6 +1,19 @@
 package hu.szamalk.modell;
 
+// 1. feladat
 public class Tabla {
+    // 2. feladat
     private char[][] T;
     private char UresCella;
+
+    // 3. feladat
+    public Tabla(char karakter) {
+        T = new char[8][8];
+        UresCella = karakter;
+        for(int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; i++) {
+                T[i][j] = karakter;
+            }
+        }
+    }
 }
